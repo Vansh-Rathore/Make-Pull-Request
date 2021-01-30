2b5c12b6b58274cdcef0974f6c119330ba2a3483
Vansh-Rathore <78263706+Vansh-Rathore@users.noreply.github.com>
2021-01-30 19:52:23 +0530

    Update Hello World.c
    Just saying Hello

--- c/Hello World.c
+++ c/Hello World.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello Anupam ");
+    printf("Hello Anupam "); //This is Vansh Rathore
+    for( int i=0; i<10; i++)
+        printf("HELLO :)");
     return 0;
 
 }
